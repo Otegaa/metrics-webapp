f336f43634e4a1681ce56f5eee606b311b65e75f
otegaa <ekiugbotega@gmail.com>
2023-05-23 16:34:13 +0200

    Implement routing for the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import Home from './components/Home';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="details" element={<DetailsPage />} />
+      </Routes>
     </div>
   );
 }
